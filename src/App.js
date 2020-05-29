de347d242364e5e541b580f50f3bc3c3ae7785a7
Soban Farhan <soban.farhan@dcmail.ca>
2020-05-29 02:42:52 -0400

    Character Display
    Added display for featured and all character list. 
    On click for any card shows the detail for character.
    
    Detail includes stats, info, personal info.

--- src/App.js
+++ src/App.js
@@ -95,19 +95,21 @@ import "./App.css"
       this.state = {  result: null, 
                       isLoaded: false,
                       show: false,
-                      data: null };
+                      data: null,
+                      allData: data,
+                      scroll: 18 };
     }
 
     componentDidMount() {
       var arr = [];
-      [60, 507, 280, 123, 487, 489].map(r => {
+      [60, 502, 276, 120, 482, 484].map(r => {
         arr.push([data[r][0], data[r][1], data[r][2]]);
       });
       this.setState({ result: arr, isLoaded: true })
     }
 
-    async handleClick(id) {
-      await fetch('https://superheroapi.com/api/2965247393590510/' + id.toString())
+    handleClick(id) {
+      fetch('https://superheroapi.com/api/2965247393590510/' + id.toString())
       .then(res => res.json())
       .then((result) => {
         this.setState({ 
@@ -122,16 +124,52 @@ import "./App.css"
       if(!this.state.isLoaded){
         return <div> Loading..... </div>
       } else {
-        return  <Row>
-                  { this.state.result.map( x => {
-                    return  <Col xs={6} lg={2} className='p-2'>
-                              <div className="border rounded h-100 shadow" onClick={ () => this.handleClick(x[0]) }>
-                                <Card className="img-container" style={{ backgroundImage: 'url('+ x[2] +')' }}>
-                                </Card>
-                                <p className="p-3" > {x[1]} </p>
-                              </div>
-                            </Col>
-                  })}
+        return  <>
+                  <Row>
+                    <Col xl={12}>
+                        <p className="p-2"></p>
+                        <h5 className="card-title m-0" style={{ fontSize: "1.5rem" }}>FEATURED CHARACTERS</h5>
+                        <hr className="hr"/>
+                        <p class="p-2 m-0"></p>
+                    </Col>
+                  </Row>
+                  <Row>
+                    { this.state.result.map( x => {
+                      return  <Col xs={6} lg={2} className='p-2'>
+                                <div className="border rounded h-100 shadow" onClick={ () => this.handleClick(x[0]) }>
+                                  <Card className="img-container" style={{ backgroundImage: 'url('+ x[2] +')' }}>
+                                  </Card>
+                                  <p className="p-3" > {x[1]} </p>
+                                </div>
+                              </Col>
+                    })}
+                  </Row>
+                  <Row>
+                    <Col xl={12}>
+                        <p className="p-2"></p>
+                        <h5 className="card-title m-0" style={{ fontSize: "1.5rem" }}>CHARACTERS LIST</h5>
+                        <hr className="hr"/>
+                        <p class="p-2 m-0"></p>
+                    </Col>
+                  </Row>
+                  <Row>
+                    { this.state.allData.slice(0, this.state.scroll ).map( x => {
+                      return  <Col xs={6} lg={2} className='p-2'>
+                                <div className="border rounded h-100 shadow" onClick={ () => this.handleClick(x[0]) }>
+                                  <Card className="img-container" style={{ backgroundImage: 'url('+ x[2] +')' }}>
+                                  </Card>
+                                  <p className="p-3" > {x[1]} </p>
+                                </div>
+                              </Col>
+                    })}
+                    <Col xl={12}>
+                      <p className="p-1"></p>
+                      <p className="p-2 text-center">
+                          <button className="btn btn-md btn-primary" onClick={() => this.setState({ scroll: this.state.scroll + 18 }) }> Load More <i className="fas fa-sync-alt"></i> </button>
+                      </p>
+                    </Col>
+                  </Row>
+                  <Row>
                     <Modal
                       size="xl"
                       show={this.state.show}
@@ -146,41 +184,95 @@ import "./App.css"
                               <img src={this.state.data?.image.url} width="100%" />
                             </Card>
                           </Col>
-                          <Col xs={9} lg={5}>
-                            <h5 className="card-title m-0" style={{ fontSize: "1.6rem"}}>{this.state.data?.name}</h5>
-                            <hr/>
+                          <Col xs={9} lg={7}>
+                            <h5 className="card-title m-0" style={{ fontSize: "1.6rem" }}>{this.state.data?.name} ({this.state.data?.id})</h5>
+                            <hr className="hr"/>
                             <p class="p-1 m-0"></p>
                             <Row>
                               <Col lg={6}>
                                 <p className="p-1 m-0">
-                                  <b><span style={{ fontSize: "13px"}}>Full Name:</span></b><br/>
+                                  <b><span style={{ fontSize: "13px"}}>Full Name</span></b><br/>
                                   <h5 className="card-title m-0" style={{ fontSize: "15px" }}>{this.state.data?.biography['full-name']}</h5>
                                 </p>
                               </Col>
                               <Col lg={6}>
                                 <p className="p-1 m-0">
-                                  <b><span style={{ fontSize: "13px"}}>Alter Ego:</span></b><br/>
+                                  <b><span style={{ fontSize: "13px"}}>Alter Ego</span></b><br/>
                                   <h5 className="card-title m-0" style={{ fontSize: "15px" }}>{this.state.data?.biography['alter-egos']}</h5>
                                 </p>
                               </Col>
                               <Col lg={12}>
                                 <p className="p-1 m-0">
-                                  <b><span style={{ fontSize: "13px"}}>Place of Birth:</span></b><br/>
+                                  <b><span style={{ fontSize: "13px"}}>Place of Birth</span></b><br/>
                                   <h5 className="card-title m-0" style={{ fontSize: "15px" }}>{this.state.data?.biography['place-of-birth']}</h5>
+                                </p>
+                              </Col>
+                              <Col lg={6}>
+                                <p className="p-1 m-0">
+                                  <b><span style={{ fontSize: "13px"}}>Occupation</span></b><br/>
+                                  <h5 className="card-title m-0" style={{ fontSize: "15px" }}>{this.state.data?.work.occupation}</h5>
+                                </p>
+                              </Col>
+                              <Col lg={6}>
+                                <p className="p-1 m-0">
+                                  <b><span style={{ fontSize: "13px"}}>Bases (Top 3)</span></b><br/>
+                                  <h5 className="card-title m-0" style={{ fontSize: "15px" }}>{this.state.data?.work.base.split(",").slice(0, 3).map(x => <li> {x} </li>)}</h5>
+                                </p>
+                              </Col>
+                              <Col lg={12}>
+                                <hr/>
+                              </Col>
+                              <Col lg={6}>
+                                <p className="p-1 m-0">
+                                  <b><span style={{ fontSize: "13px"}}>Publisher</span></b><br/>
+                                  <h5 className="card-title m-0" style={{ fontSize: "15px" }}>{this.state.data?.biography.publisher}</h5>
+                                </p>
+                              </Col>
+                              <Col lg={6}>
+                                <p className="p-1 m-0">
+                                  <b><span style={{ fontSize: "13px"}}>Aliases (Top 3)</span></b><br/>
+                                  <h5 className="card-title m-0" style={{ fontSize: "15px" }}>{this.state.data?.biography.aliases.slice(0, 3).map(x => <li> {x} </li>) }</h5>
+                                </p>
+                              </Col>
+                              <Col lg={12}>
+                                <hr/>
+                              </Col>
+                              <Col xs={6} lg={2}>
+                                <p className="p-1 m-0">
+                                  <b><span style={{ fontSize: "13px"}}>Height</span></b><br/>
+                                  <h5 className="card-title m-0" style={{ fontSize: "15px" }}>{this.state.data?.appearance.height[0]}"</h5>
+                                </p>
+                              </Col>
+                              <Col xs={6} lg={2}>
+                                <p className="p-1 m-0">
+                                  <b><span style={{ fontSize: "13px"}}>Weight</span></b><br/>
+                                  <h5 className="card-title m-0" style={{ fontSize: "15px" }}>{this.state.data?.appearance.weight[0]}</h5>
+                                </p>
+                              </Col>
+                              <Col xs={6} lg={2}>
+                                <p className="p-1 m-0">
+                                  <b><span style={{ fontSize: "13px"}}>Race</span></b><br/>
+                                  <h5 className="card-title m-0" style={{ fontSize: "15px" }}>{this.state.data?.appearance.race}</h5>
+                                </p>
+                              </Col>
+                              <Col xs={6} lg={2}>
+                                <p className="p-1 m-0">
+                                  <b><span style={{ fontSize: "13px"}}>Gender</span></b><br/>
+                                  <h5 className="card-title m-0" style={{ fontSize: "15px" }}>{this.state.data?.appearance.gender}</h5>
                                 </p>
                               </Col>
                             </Row>
                           </Col>
-                          <Col xs={6} lg={3}>
-
+                        </Row>
+                        <Row>
+                          <Col lg={11}>
+                            <hr className="hr"/>
                           </Col>
                         </Row>
-                        <p>
-                          
-                        </p>
                       </Modal.Body>
                     </Modal>
-                </Row>
+                  </Row>
+                </>
               }
             }
   }
